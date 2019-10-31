cafc10265908948c1dc6a79ef8254219764428bb
Kevin Cromwell <>
2019-10-30 19:32:14 -0500

    Changes to be committed: 	modified:   data.js 	modified:   index.html

--- data.js
+++ data.js
@@ -1,5 +1,7 @@
 // javascript data file
 
+const aspect = 1/1;
+
 const screens_data = new Array(
     {
         background : '#000000',
@@ -10,7 +12,7 @@ const screens_data = new Array(
     },
     {
         background : '#000000',
-        content : '<h3>Good Work!</h3><p>During hot work activities, you must keep control over the work site and constantly watch out for unsafe conditions.</p><p>Avoiding fire is your main concern.</p><p><i>Click on the Next button to continue the module.</i></p><button onclick="state.setState(\'game\', REPLAY )">REPLAY</button>',
+        content : '<h3>Good Work!</h3><p></p><button onclick="state.setState(\'game\', REPLAY )">REPLAY</button>',
         mode : 3,
         show : false,
         audio : '2C_450.mp3',
@@ -19,20 +21,31 @@ const screens_data = new Array(
 
 const walls_data = new Array
 (
-    { posx:  0, posy:  3, thickness : 1.25, lengthx: 100, lengthy:  0 },
-    { posx: 14, posy: 18, thickness : 1.25, lengthx:   0, lengthy: 80 },
-    { posx: 28, posy:  3, thickness : 1.25, lengthx:   0, lengthy: 14 },
-    { posx: 28, posy: 17, thickness : 1.25, lengthx:  14, lengthy:  0 },
-    { posx: 56, posy: 17, thickness : 1.25, lengthx:  14, lengthy:  0 },
-    { posx: 56, posy: 17, thickness : 1.25, lengthx:   0, lengthy: 87 },
-    { posx: 28, posy: 31, thickness : 1.25, lengthx:  28, lengthy:  0 },
-    { posx: 28, posy: 31, thickness : 1.25, lengthx:   0, lengthy: 15 },
-    { posx: 42, posy: 43, thickness : 1.25, lengthx:   0, lengthy: 67 },
-    { posx: 70, posy: 29, thickness : 1.25, lengthx:  14, lengthy:  0 },
-    { posx: 70, posy: 43, thickness : 1.25, lengthx:   0, lengthy: 57 },
-    { posx: 84, posy:  3, thickness : 1.25, lengthx:   0, lengthy: 42 },    
+    { posx: 20, posy: 20, thickness : 1.25, lengthx:  60, lengthy:  0 },
+    { posx: 20, posy: 20, thickness : 1.25, lengthx:   0, lengthy: 60 },
+    { posx: 80, posy: 20, thickness : 1.25, lengthx:   0, lengthy: 60 },
+    { posx: 20, posy: 80, thickness : 1.25, lengthx:  60, lengthy:  0 },
 );
 
+const hits_data = new Array
+(
+    { posx:  0, posy:  0, width: 20, height: 20, on: 'blow_hit',      frame : 0 },
+    { posx: 20, posy:  0, width: 20, height: 20, on: 'blow_hit',      frame : 0 },
+    { posx: 40, posy:  0, width: 20, height: 20, on: 'blow_hit',      frame : 0 },
+    { posx: 60, posy:  0, width: 20, height: 20, on: 'blow_hit',      frame : 0 },
+    { posx: 80, posy:  0, width: 20, height: 20, on: 'blow_hit',      frame : 0 },
+    { posx:  0, posy: 20, width: 20, height: 20, on: 'blow_hit',      frame : 0 },
+    { posx:  0, posy: 40, width: 20, height: 20, on: 'blow_hit',      frame : 0 },
+    { posx:  0, posy: 60, width: 20, height: 20, on: 'blow_hit',      frame : 0 },
+    { posx:  0, posy: 80, width: 20, height: 20, on: 'blow_hit',      frame : 0 },
+    { posx: 80, posy: 20, width: 20, height: 20, on: 'blow_hit',      frame : 0 },
+    { posx: 80, posy: 40, width: 20, height: 20, on: 'blow_hit',      frame : 0 },
+    { posx: 80, posy: 60, width: 20, height: 20, on: 'blow_hit',      frame : 0 },
+    { posx: 80, posy: 80, width: 20, height: 20, on: 'blow_hit',      frame : 0 },
+    { posx: 20, posy: 80, width: 20, height: 20, on: 'blow_hit',      frame : 0 },
+    { posx: 40, posy: 80, width: 20, height: 20, on: 'blow_hit',      frame : 0 },
+    { posx: 60, posy: 80, width: 20, height: 20, on: 'blow_congrats', frame : 0 },
+);
 
 const pc_instructions = '<b>Use the appropriate keys on the keyboard to move through the maze.</b>';
 const mobile_instructions = '<b>Use the direction pad to move through the maze.</b>';
